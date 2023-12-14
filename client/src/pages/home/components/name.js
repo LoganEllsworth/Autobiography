750bce7971f76e5254f4fc7cf2e0fa3680cf21f4
Logan Ellsworth <97066574+LoganEllsworth@users.noreply.github.com>
2023-12-13 17:55:50 -0700

    Remove default image

--- client/src/pages/home/components/name.js
+++ client/src/pages/home/components/name.js
@@ -10,7 +10,7 @@ const Name = ({ variants, isMobile }) => {
                 <Paper className={'basicPaper'}>
                     <Grid container direction='row' justifyContent={isMobile ? 'center' : 'space-between'}>
                         {isMobile && <Grid item sx={{justifySelf: 'center'}}>
-                            <img className='circularImage' src={'https://cdn.discordapp.com/attachments/504065122713468939/1183297490053775391/dem-headshot.jpg?ex=6587d2aa&is=65755daa&hm=a83d26c7904934d67db63296cfbc42445b0df465c55cbe5793430cd769d6b2c0&'} alt={"My Headshot"} />
+                            {/* <img className='circularImage' src={'https://cdn.discordapp.com/attachments/504065122713468939/1183297490053775391/dem-headshot.jpg?ex=6587d2aa&is=65755daa&hm=a83d26c7904934d67db63296cfbc42445b0df465c55cbe5793430cd769d6b2c0&'} alt={"My Headshot"} /> */}
                         </Grid>}
                         <Grid item>
                             <Typography variant='h4' textAlign={isMobile ? 'center' : null}>Logan Ellsworth</Typography>
@@ -18,7 +18,7 @@ const Name = ({ variants, isMobile }) => {
                             <Link href='mailto:loganellsworth20@outlook.com'><Typography variant='subtitle1' textAlign={isMobile ? 'center' : null}>loganellsworth20@outlook.com</Typography></Link>
                         </Grid>
                         {!isMobile && <Grid item>
-                            <img className='circularImage' src={'https://cdn.discordapp.com/attachments/504065122713468939/1183297490053775391/dem-headshot.jpg?ex=6587d2aa&is=65755daa&hm=a83d26c7904934d67db63296cfbc42445b0df465c55cbe5793430cd769d6b2c0&'} alt={"My Headshot"} />
+                            {/* <img className='circularImage' src={'https://cdn.discordapp.com/attachments/504065122713468939/1183297490053775391/dem-headshot.jpg?ex=6587d2aa&is=65755daa&hm=a83d26c7904934d67db63296cfbc42445b0df465c55cbe5793430cd769d6b2c0&'} alt={"My Headshot"} /> */}
                         </Grid>}
                     </Grid>
                 </Paper>
