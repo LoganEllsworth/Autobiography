1fe9e83cd1e04ae5c3596c2f1a3b459af9424f56
Logan Ellsworth <97066574+LoganEllsworth@users.noreply.github.com>
2023-12-15 17:25:26 -0700

    Google Analytics & Headshot

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,11 @@ import { AppStore } from './appStore';
 import ScrollToTop from './pages/components/scrollToTop';
 import Nav from './pages/components/nav';
 import Footer from './pages/components/footer';
+import ReactGA from 'react-ga4';
 
+const TESTING_TRACKING_ID = 'G-L9SZ5RVC03';
+const HOT_TRACKING_ID = 'G-54K5616D7P';
+ReactGA.initialize(process.env.NODE_ENV === 'development' ? TESTING_TRACKING_ID : HOT_TRACKING_ID);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
